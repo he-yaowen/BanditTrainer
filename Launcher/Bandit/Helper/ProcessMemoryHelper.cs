68d3cfa90fc8ec07e8ce6e57abb8f601024b09a1
HE Yaowen <he.yaowen@hotmail.com>
2019-02-11 19:54:37 +0800

    Add helper to operate process memory

---
+++ Launcher/Bandit/Helper/ProcessMemoryHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Bandit.Helper
+{
+    public abstract class ProcessMemoryHelper
+    {
+        public const int PROCESS_ALL_ACCESS = 0x1F0FFF;
+        public const int PROCESS_VM_READ = 0x0010;
+        public const int PROCESS_VM_WRITE = 0x0020;
+
+        #region API
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll")]
+        private static extern void CloseHandle(IntPtr hObject);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int nSize, IntPtr lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, int[] lpBuffer, int nSize, IntPtr lpNumberOfBytesWritten);
+
+        #endregion
+    }
+}
