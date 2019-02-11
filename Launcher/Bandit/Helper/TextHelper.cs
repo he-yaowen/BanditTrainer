e5c6ef39bfaabad1e00afeef12ec4c5e17169e3c
HE Yaowen <he.yaowen@hotmail.com>
2019-02-11 20:40:43 +0800

    Add helper to operate text

---
+++ Launcher/Bandit/Helper/TextHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bandit.Helper
+{
+    public class TextHelper
+    {
+        public static string ConvertUTF8(byte[] bytes)
+        {
+            Encoding big5 = Encoding.GetEncoding("Big5");
+            UTF8Encoding utf8 = new UTF8Encoding();
+
+            string result = utf8.GetString(Encoding.Convert(big5, utf8, bytes)).Trim('\0');
+
+            if (result == "0") {
+                result = "";
+            }
+
+            return result;
+        }
+    }
+}
