0bd058823a69daba6c5f7b51513f6619147ed276
HE Yaowen <he.yaowen@hotmail.com>
2019-02-13 20:03:55 +0800

    Add classes for runtime

---
+++ Launcher/Bandit/Runtime/GameProcess.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using Bandit;
+
+namespace Bandit.Runtime
+{
+    public abstract class GameProcess
+    {
+        public List<Force> Forces;
+        public List<Hero> Heroes;
+        public List<Prefecture> Prefectures;
+
+        protected abstract void LoadForces(Process process);
+        protected abstract void LoadHeroes(Process process);
+        protected abstract void LoadPrefectures(Process process);
+
+        public GameProcess()
+        {
+            Forces = new List<Force>();
+            Heroes = new List<Hero>();
+            Prefectures = new List<Prefecture>();
+        }
+
+        public void AttachProcess(Process process)
+        {
+            LoadForces(process);
+            LoadHeroes(process);
+            LoadPrefectures(process);
+        }
+    }
+}
---
+++ Launcher/Bandit/Runtime/GameProcess_Win32_zhTW.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Bandit.Runtime
+{
+    public class GameProcess_Win32_zhTW : GameProcess
+    {
+        protected override void LoadForces(Process process)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void LoadHeroes(Process process)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void LoadPrefectures(Process process)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
